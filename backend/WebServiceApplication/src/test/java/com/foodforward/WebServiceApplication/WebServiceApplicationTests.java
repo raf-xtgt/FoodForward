b2b937ff4f1683af5bc37659d1c9c8d0ff165336
raf-xtgt <rafaquat9905@gmail.com>
2024-08-03 17:18:43 +0600

    BE setup

---
+++ backend/WebServiceApplication/src/test/java/com/foodforward/WebServiceApplication/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodforward.WebServiceApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
