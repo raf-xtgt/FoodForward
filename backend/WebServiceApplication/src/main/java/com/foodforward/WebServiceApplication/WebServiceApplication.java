607fca3f5c771d77991480fa1b4d3d1563478156
raf-xtgt <rafaquat9905@gmail.com>
2024-08-07 10:28:49 +0600

    Separation into screens and states

--- backend/WebServiceApplication/src/main/java/com/foodforward/WebServiceApplication/WebServiceApplication.java
+++ backend/WebServiceApplication/src/main/java/com/foodforward/WebServiceApplication/WebServiceApplication.java
@@ -1,12 +1,10 @@
 package com.foodforward.WebServiceApplication;
 
-import com.google.firebase.cloud.FirestoreClient;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 
