662b533df0de9e33d04f0dfc04f94d1b4340e499
raf-xtgt <rafaquat9905@gmail.com>
2024-10-02 02:22:02 +0600

    Updated home page

--- frontend/food_forward_app/lib/screens/home/home-screen.dart
+++ frontend/food_forward_app/lib/screens/home/home-screen.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:food_forward_app/screens/home/widgets/welcome-card.dart';
 import 'package:food_forward_app/screens/home/widgets/donate-ngo-card.dart';
-import 'package:food_forward_app/screens/home/widgets/community-card.dart';
 import 'package:food_forward_app/screens/home/widgets/recipe-card.dart';
 import 'package:food_forward_app/screens/home/widgets/scan-receipt-card.dart';
 import 'package:food_forward_app/screens/home/widgets/food-stock-mgmt-card.dart';
-import 'package:food_forward_app/screens/home/widgets/food-stock-stats-card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -16,49 +14,36 @@ class HomeScreen extends StatelessWidget {
       body: Container(
         color: const Color(0xFFFFF4EC), // Setting the background color
        
-       child: Center(
+       child: const Center(
                 child: SizedBox(
                 width: double.infinity,
                 height: 850.0, // Increase this value to change the height of the camera preview
                 child: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(16.0),
+            padding: EdgeInsets.all(16.0),
             child: Column(
               children: [
-                const WelcomeCard(),
-                const SizedBox(height: 16.0),
+                WelcomeCard(),
+                SizedBox(height: 16.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
+                  children: [
                     Expanded(child: DonateNgoCard()),
                     SizedBox(width: 16.0),
                     Expanded(child: ScanReceiptCard()),
                   ],
                 ),
-                const SizedBox(height: 16.0),
+                SizedBox(height: 16.0),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: const [
+                  children: [
                     Expanded(child: FoodStockMgmtCard()),
                     SizedBox(width: 16.0),
                     Expanded(child: ReceipeCard()),
                   ],
                 ),
-                const SizedBox(height: 16.0),
-                const Text("Hear from the community"),
-                const SizedBox(height: 16.0),
-                SizedBox(
-                  height: 120.0, // Set a fixed height for horizontal scroll area
-                  child: ListView(
-                    scrollDirection: Axis.horizontal,
-                    children: const [
-                      CommunityCard(),
-                      CommunityCard(),
-                      CommunityCard(),
-                      CommunityCard(),
-                    ],
-                  ),
-                ),
+                SizedBox(height: 16.0),
+
               ],
             ),
           ),
