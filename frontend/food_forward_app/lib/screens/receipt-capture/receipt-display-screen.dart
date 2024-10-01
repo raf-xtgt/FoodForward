13116bbdeaa5238c5dce9d67a7831ccf836da136
raf-xtgt <rafaquat9905@gmail.com>
2024-10-02 02:18:16 +0600

    Standardized all ui

--- frontend/food_forward_app/lib/screens/receipt-capture/receipt-display-screen.dart
+++ frontend/food_forward_app/lib/screens/receipt-capture/receipt-display-screen.dart
@@ -9,36 +9,52 @@ class ReceiptDisplayScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Display the Picture')),
-      body: Column(
-        children: [
-          Expanded(
-            child: Image.file(File(imagePath)),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // Discard the image and go back to the previous screen.
-                    Navigator.of(context).pop(false); // Return false
-                  },
-                  child: const Text('Discard'),
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    // Save the image and go back to the previous screen.
-                    Navigator.of(context).pop(true); // Return true
-                  },
-                  child: const Text('Continue'),
-                ),
-              ],
+      backgroundColor: const Color(0xFFFFF4EC),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF2B72A8),
+        title: const Text('Captured Receipts',
+          style: TextStyle(
+              color: Colors.white, // Set the desired color for the title text
             ),
-          ),
-        ],
+          )
+        ),
+          body: Column(
+          children: [
+            Expanded(
+              child: Image.file(File(imagePath)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // Discard the image and go back to the previous screen.
+                      Navigator.of(context).pop(false); // Return false
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white, backgroundColor: Colors.lightBlue, // White text color
+                    ),
+                    child: const Text('Discard'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Save the image and go back to the previous screen.
+                      Navigator.of(context).pop(true); // Return true
+                    },
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.white, backgroundColor: Colors.lightBlue, // White text color
+                    ),
+                    child: const Text('Continue'),
+                  ),
+                ],
+              ),
+            ),
+          ],
       ),
+
+    
     );
   }
 }
